ce8a9c81233454aea7df115c47bd3c129c74ad0f
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2014-02-11 11:03:26 +0100

    ajout du hook sur builddoc pour ajouter l'incoterm dans la note public

--- class/actions_incoterm.class.php
+++ class/actions_incoterm.class.php
@@ -10,7 +10,7 @@ class ActionsIncoterm
      
     function doActions($parameters, &$object, &$action, $hookmanager) 
     {
-    	global $db;
+    	global $langs, $db, $conf, $user;
 		/*echo '<pre>';
 		print_r($object);
 		echo '</pre>';*/
@@ -18,6 +18,91 @@ class ActionsIncoterm
         if($action == "validmodincoterm"){
 			if(isset($_REQUEST['incoterms']) && !empty($_REQUEST['incoterms'])){
 				$db->query('UPDATE '.MAIN_DB_PREFIX.$object->table_element.' SET fk_incoterms = '.$_REQUEST['incoterms'].' WHERE rowid = '.$object->id);
+			}
+		}
+		
+		//Ajout au document pdf
+		if (in_array('ordercard',explode(':',$parameters['context'])) || in_array('propalcard',explode(':',$parameters['context']))
+			|| in_array('expeditioncard',explode(':',$parameters['context'])) || in_array('invoicecard',explode(':',$parameters['context']))){
+			
+			/*echo '<pre>';
+			print_r($action);
+			echo '</pre>';exit;*/
+			
+        	if ($action == 'builddoc')
+			{
+				dol_include_once('/core/lib/product.lib.php');
+				
+				// *************************************
+				// On modifie les infos qu'on souhaite
+				// *************************************
+				
+				// 1 - Dans le document
+				
+				//Ajout des Incoterms dans la note public
+				$resl = $db->query('SELECT ci.code
+						FROM '.MAIN_DB_PREFIX.'c_incoterms as ci
+							LEFT JOIN '.MAIN_DB_PREFIX.$object->table_element.' as te ON (te.fk_incoterms = ci.rowid)
+						WHERE te.rowid = '.$object->id);
+				$res = $db->fetch_object($resl);
+				
+				if($res){
+					$object->note_public .= "\nIncoterm : ".$res->code;
+				}
+				
+				// ***********************************************
+				// On reproduis le traitement standard de dolibarr
+				// ***********************************************
+				
+				if (GETPOST('model'))
+				{
+					$object->setDocModel($user, GETPOST('model'));
+				}
+				
+				// Define output language
+				$outputlangs = $langs;
+				if (! empty($conf->global->MAIN_MULTILANGS))
+				{
+					$outputlangs = new Translate("",$conf);
+					$newlang=(GETPOST('lang_id') ? GETPOST('lang_id') : $object->client->default_lang);
+					$outputlangs->setDefaultLang($newlang);
+				}
+				$ret=$object->fetch($id);    // Reload to get new records
+				
+				/*echo '<pre>';
+				print_r($object);
+				echo '</pre>'; exit;*/
+				
+				switch ($object->element) {
+					case 'propal':
+						$result= propale_pdf_create($db, $object, $object->modelpdf, $outputlangs, $hidedetails, $hidedesc, $hideref);
+						break;
+					case 'facture':
+						$result= facture_pdf_create($db, $object, $object->modelpdf, $outputlangs, $hidedetails, $hidedesc, $hideref);
+						break;
+					case 'commande':
+						$result= commande_pdf_create($db, $object, $object->modelpdf, $outputlangs, $hidedetails, $hidedesc, $hideref);
+						break;
+					case 'shipping':
+						$result= expedition_pdf_create($db, $object, $object->modelpdf, $outputlangs);
+						break;
+					
+					default:
+						
+						break;
+				}
+				
+				
+				if ($result <= 0)
+				{
+					dol_print_error($db,$result);
+					exit;
+				}
+				elseif(!in_array('ordercard',explode(':',$parameters['context'])))
+				{
+					header('Location: '.$_SERVER["PHP_SELF"].'?id='.$object->id.(empty($conf->global->MAIN_JUMP_TAG)?'':'#builddoc'));
+					exit;
+				}
 			}
 		}
 		
