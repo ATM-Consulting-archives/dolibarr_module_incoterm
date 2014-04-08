e845bae13916afac00143de99ef16a6652313c62
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2014-04-08 11:54:30 +0200

    modification config pour utilisation abricot

--- config.default.php
+++ config.default.php
@@ -1,30 +1,25 @@
 <?php
 
-define('ROOT','/var/www/dolibarr/htdocs/');
-define('COREROOT','/var/www/ATM/atm-core/');
-define('COREHTTP','http://127.0.0.1/ATM/atm-core/');
-define('HTTP','http://127.0.0.1/dolibarr/');
+	if(is_file('../main.inc.php'))$dir = '../';
+	else  if(is_file('../../../main.inc.php'))$dir = '../../../';
+	else  if(is_file('../../../../main.inc.php'))$dir = '../../../../';
+	else  if(is_file('../../../../../main.inc.php'))$dir = '../../../../../';
+	else $dir = '../../';
 
-if(!defined('INC_FROM_DOLIBARR') && defined('INC_FROM_CRON_SCRIPT')) {
-	include_once(ROOT."master.inc.php");
-}
-elseif(!defined('INC_FROM_DOLIBARR')) {
-	include_once(ROOT."main.inc.php");
-} else {
-	global $dolibarr_main_db_host, $dolibarr_main_db_name, $dolibarr_main_db_user, $dolibarr_main_db_pass;
-}
+	if(!defined('INC_FROM_DOLIBARR') && defined('INC_FROM_CRON_SCRIPT')) {
+		include($dir."master.inc.php");
+	}
+	elseif(!defined('INC_FROM_DOLIBARR')) {
+		include($dir."main.inc.php");
+	} else {
+		global $dolibarr_main_db_host, $dolibarr_main_db_name, $dolibarr_main_db_user, $dolibarr_main_db_pass;
+	}
+	if(!defined('DB_HOST')) {
+		define('DB_HOST',$dolibarr_main_db_host);
+		define('DB_NAME',$dolibarr_main_db_name);
+		define('DB_USER',$dolibarr_main_db_user);
+		define('DB_PASS',$dolibarr_main_db_pass);
+		define('DB_DRIVER',$dolibarr_main_db_type);
+	}
 
-if(!empty($dolibarr_main_db_host)) {
-	define('DB_HOST',$dolibarr_main_db_host);
-	define('DB_NAME',$dolibarr_main_db_name);
-	define('DB_USER',$dolibarr_main_db_user);
-	define('DB_PASS',$dolibarr_main_db_pass);
-	define('DB_DRIVER','mysqli');
-}
-
-define('DOL_PACKAGE', true);
-define('USE_TBS', true);
-
-require(COREROOT.'inc.core.php');
- 
-define('DOL_ADMIN_USER', 'admin');
+	dol_include_once('/abricot/inc.core.php');
